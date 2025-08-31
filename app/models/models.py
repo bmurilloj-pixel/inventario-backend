03fb83e613aa61b74a5f958e07d5e18831b6f692
bmurilloj-pixel <bmurilloj@gmail.com>
2025-08-31 18:32:11 -0500

    Update models.py

--- app/models/models.py
+++ app/models/models.py
@@ -26,3 +26,22 @@ class Movement(Base):
     type = Column(String)  # entrada o salida
     date = Column(DateTime, default=datetime.datetime.utcnow)
     product = relationship("Product")
+# app/models.py
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.models import Base  # importa la Base del package
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="user")
+    local_kw = Column(String, nullable=True)
+
+class Inventory(Base):
+    __tablename__ = "inventory"
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    note = Column(String, nullable=True)
+    timestamp = Column(DateTime, server_default=func.now(), nullable=False)
